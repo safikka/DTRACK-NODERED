34cf2df9c47772ee701d089afb8b94d105edd191
mahdy-kpp <sigmatech>
2022-11-28 22:15:38 +0700

    Lupa command pindah flow

--- update.sh
+++ update.sh
@@ -6,13 +6,13 @@ if test -f "/home/pi/.node-red/flows.json";
 then
     rm /home/pi/.node-red/flows.json
     echo "Hapus flow lama: OK!"
+fi
 
-    if cp DTRACK-GPS.db /home/pi/ && cp DTRACK-SPEED.db /home/pi/ && cp settingGPS.json /home/pi/ && cp -R node-red-contrib-kafka-manager /home/pi/.node-red/node_modules/ && cp -R node-red-contrib-nmea /home/pi/.node-red/node_modules/ && cp -R node-red-contrib-kafkajs /home/pi/.node-red/node_modules/
-    then
-        echo "Sukses salin file Update!"
-    else
-        echo "Gagal $?"
-    fi
+if cp DTRACK-GPS.db /home/pi/ && cp DTRACK-SPEED.db /home/pi/ && cp settingGPS.json /home/pi/ && cp -R node-red-contrib-kafka-manager /home/pi/.node-red/node_modules/ && cp -R node-red-contrib-nmea /home/pi/.node-red/node_modules/ && cp -R node-red-contrib-kafkajs /home/pi/.node-red/node_modules/ && cp flows.json /home/pi/.node-red/
+then
+    echo "Sukses salin file Update!"
+else
+    echo "Gagal $?"
 fi
 
 if sudo systemctl restart nodered.service
