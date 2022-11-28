3a16a339cbd50b2d602b94913e17d3d827db933d
mahdy-kpp <sigmatech>
2022-11-28 11:24:19 +0700

    Menambahkan script update

--- update.sh
+++ update.sh
@@ -2,9 +2,16 @@
 
 echo "Memulai update DTRACK ..."
 
-if cp DTRACK-GPS.db /home/pi/
+if cp DTRACK-GPS.db /home/pi/ && cp DTRACK-SPEED.db /home/pi/ && cp settingGPS.json /home/pi/ && cp -R node-red-contrib-kafka-manager /home/pi/.node-red/node_modules/ && cp -R node-red-contrib-nmea /home/pi/.node-red/node_modules/
 then
-    echo "Sukses salin DTRACK-GPS.db"
+    echo "Sukses salin file Update!"
+else
+    echo "Gagal $?"
+fi
+
+if sudo systemctl restart nodered.service
+then
+    echo "Sukses restart Node-RED!"
 else
     echo "Gagal $?"
 fi
