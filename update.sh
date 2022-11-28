b2bc0779bab4bda78aaa1ecf86d9ee71e6a4adf3
mahdy-kpp <sigmatech>
2022-11-28 14:39:27 +0700

    Update command script

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 
 echo "Memulai update DTRACK ..."
 
-if cp DTRACK-GPS.db /home/pi/ && cp DTRACK-SPEED.db /home/pi/ && cp settingGPS.json /home/pi/ && cp -R node-red-contrib-kafka-manager /home/pi/.node-red/node_modules/ && cp -R node-red-contrib-nmea /home/pi/.node-red/node_modules/
+if cp DTRACK-GPS.db /home/pi/ && cp DTRACK-SPEED.db /home/pi/ && cp settingGPS.json /home/pi/ && cp -R node-red-contrib-kafka-manager /home/pi/.node-red/node_modules/ && cp -R node-red-contrib-nmea /home/pi/.node-red/node_modules/ && cp -R node-red-contrib-kafkajs /home/pi/.node-red/node_modules/
 then
     echo "Sukses salin file Update!"
 else
